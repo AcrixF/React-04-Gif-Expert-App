c9bee822c8c669b8420948032e3bc3e5e0084106
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-07-14 11:38:58 -0500

    Custom Hook Fetch Gifs, simple animations

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import { GifGridItem } from './GifGridItem';
-import { getGifs } from './helpers/GetGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setimages] = useState([]);
-
-     useEffect(() => {
-        getGifs(category)
-            .then(setimages);
-    }, [category])
+    const { data: images, loading } = useFetchGifs(category);
 
     return(
     <>
         <h3>{ category }</h3>
+
+        { loading && <p className="animate__animated animate__flash">Loading...</p>}
+
          <div className="card-grid">
             {
                 images.map((img) => 
