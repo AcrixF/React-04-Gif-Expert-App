878e75edd6e34b7b94009a71b8c73d62b7db040f
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-07-14 11:41:52 -0500

    Rename build to Docs

---
+++ docs/precache-manifest.a4b71ccebde9ef1547da6bf8cb37bfd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b3e8fb4fdcfc3790135c5d6c7655b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "153909baf8a9de9680f4",
+    "url": "/static/css/main.8fb274ff.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153909baf8a9de9680f4",
+    "url": "/static/js/main.3f516f26.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
