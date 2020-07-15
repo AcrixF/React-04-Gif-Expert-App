61c7c16371bcdd06955f4f44c6673fa80513e177
Cristian Flores <Cristian.X.Flores.-ND@disney.com>
2020-07-14 21:43:45 -0500

    Simulate submit form call, validate when a function has not been called

--- src/test/AddCategory.test.js
+++ src/test/AddCategory.test.js
@@ -5,12 +5,28 @@ import AddCategory from '../components/AddCategory';
 
 describe('should show the <AddCategory/> component', () => {
 
-    const setCategories = () => {};
-   
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories ={ setCategories } />);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories ={ setCategories } />);
+    })    
+
+
     test('should show the component snapshot', () => {
-        const wrapper = shallow(<AddCategory setCategories ={ setCategories } />);
         expect(wrapper).toMatchSnapshot();
     });
-    
-    
+
+    test('should simulate change the input', () => {
+        const value = 'Hunter x Hunter';
+        const input = wrapper.find('input');
+        input.simulate('change', { target: value });
+    });
+
+    test('should not submit the data', () => {
+        wrapper.find('form').simulate('submit', {preventDefault(){}});
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+        
 });
